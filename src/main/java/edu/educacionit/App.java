c53446c6d7ca2dbc93f0004c3635f029131b392a
dkarinarojas <d.karinarojas@gmail.com>
2019-05-06 12:33:21 -0300

    primer commit

---
+++ src/main/java/edu/educacionit/App.java
@@ -0,0 +1,21 @@
+package edu.educacionit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+ /*   private String id;
+    private String nombre;
+    
+    public void Usuario(String id, String nombre){
+        this.id=id;
+        this.nombre=nombre;
+    }*/
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
